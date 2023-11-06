fc46c57b20a8142e56d80ccaeaaa0b137cc82a6a
NanReven <nanreven@gmail.com>
2023-11-06 23:21:28 +0300

    added templates

--- salon/main/urls.py
+++ salon/main/urls.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home_page')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
